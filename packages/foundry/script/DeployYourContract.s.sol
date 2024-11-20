3a5d62b02b6b8e9aa78d332d0c9cb9dd0ddcbde4
Yahsin Huang <yahsinhuangtw@gmail.com>
2024-11-20 17:33:37 +0800

    deploy nft contract

--- packages/foundry/script/DeployYourContract.s.sol
+++ packages/foundry/script/DeployYourContract.s.sol
@@ -1,16 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/YourContract.sol";
+import "../contracts/NFT.sol";
 import "./DeployHelpers.s.sol";
 
 contract DeployYourContract is ScaffoldETHDeploy {
   // use `deployer` from `ScaffoldETHDeploy`
   function run() external ScaffoldEthDeployerRunner {
-    YourContract yourContract = new YourContract(deployer);
+    NFT nft = new NFT(deployer);
     console.logString(
       string.concat(
-        "YourContract deployed at: ", vm.toString(address(yourContract))
+        "NFT deployed at: ", vm.toString(address(nft))
       )
     );
   }
